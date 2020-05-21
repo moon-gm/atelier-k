2412732f0110f6be09d04819ddcaeaab43ea455c
akinaga <akinaga@freedomland.jp>
2020-05-21 11:28:31 +0900

    ギャラリーページ作成(2013)

---
+++ src/config/config_gallery-2013.js
@@ -0,0 +1,112 @@
+// 2013年のギャラリーの設定
+export default [
+    [
+        // 1月
+        {
+            'month': 'January',
+            'fileName': 'stamp-1.png',
+            'altName': 'くうちゃんスタンプ画像１',
+            'comment': 'part.1 うさぎかぶり',
+        },
+        {
+            'month': 'January',
+            'fileName': 'stamp-1.png',
+            'altName': 'くうちゃんスタンプ画像１',
+            'comment': 'part.1 うさぎかぶり',
+        },
+        {
+            'month': 'January',
+            'fileName': 'stamp-1.png',
+            'altName': 'くうちゃんスタンプ画像１',
+            'comment': 'part.1 うさぎかぶり',
+        },
+        {
+            'month': 'January',
+            'fileName': 'stamp-1.png',
+            'altName': 'くうちゃんスタンプ画像１',
+            'comment': 'part.1 うさぎかぶり',
+        },
+
+
+    ],
+    [
+        // 2月
+        {
+            'month': 'February',
+            'fileName': 'stamp-1.png',
+            'altName': 'くうちゃんスタンプ画像１',
+            'comment': 'part.1 うさぎかぶり',
+        },
+        {
+            'month': 'February',
+            'fileName': 'stamp-1.png',
+            'altName': 'くうちゃんスタンプ画像１',
+            'comment': 'part.1 うさぎかぶり',
+        },
+        {
+            'month': 'February',
+            'fileName': 'stamp-1.png',
+            'altName': 'くうちゃんスタンプ画像１',
+            'comment': 'part.1 うさぎかぶり',
+        },
+        {
+            'month': 'February',
+            'fileName': 'stamp-1.png',
+            'altName': 'くうちゃんスタンプ画像１',
+            'comment': 'part.1 うさぎかぶり',
+        },
+        {
+            'month': 'February',
+            'fileName': 'stamp-1.png',
+            'altName': 'くうちゃんスタンプ画像１',
+            'comment': 'part.1 うさぎかぶり',
+        },
+    ],
+    [
+        // 3月
+        {
+            'month': 'March',
+            'fileName': 'stamp-1.png',
+            'altName': 'くうちゃんスタンプ画像１',
+            'comment': 'part.1 うさぎかぶり',
+        },
+        {
+            'month': 'March',
+            'fileName': 'stamp-1.png',
+            'altName': 'くうちゃんスタンプ画像１',
+            'comment': 'part.1 うさぎかぶり',
+        },
+        {
+            'month': 'March',
+            'fileName': 'stamp-1.png',
+            'altName': 'くうちゃんスタンプ画像１',
+            'comment': 'part.1 うさぎかぶり',
+        },
+        {
+            'month': 'March',
+            'fileName': 'stamp-1.png',
+            'altName': 'くうちゃんスタンプ画像１',
+            'comment': 'part.1 うさぎかぶり',
+        },
+        {
+            'month': 'March',
+            'fileName': 'stamp-1.png',
+            'altName': 'くうちゃんスタンプ画像１',
+            'comment': 'part.1 うさぎかぶり',
+        },
+        {
+            'month': 'March',
+            'fileName': 'stamp-1.png',
+            'altName': 'くうちゃんスタンプ画像１',
+            'comment': 'part.1 うさぎかぶり',
+        },
+        {
+            'month': 'March',
+            'fileName': 'stamp-1.png',
+            'altName': 'くうちゃんスタンプ画像１',
+            'comment': 'part.1 うさぎかぶり',
+        },
+
+    ],
+];
+
--- src/pages/2013.js
+++ src/pages/2013.js
@@ -1,6 +1,9 @@
 import React from "react"
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Img from "../components/parts/image"
+import gallery2013Config from "../config/config_gallery-2013.js"
 import Styles from "../sass/module/pages/common-gallery.module.scss"
 
 const Gallery2013 = () => (
@@ -9,31 +12,53 @@ const Gallery2013 = () => (
     <h1
         className={ Styles.title }
     >
-        〜Gallery 2013〜
+        〜 Gallery 2013 〜
     </h1>
-    <div
-        className={ Styles.flexBox }
-    >
-        {/* 画像＋コメントのコンテナ */}
-        <div
-            className={ Styles.container }
-        >
-            {/* 画像コンテンツ */}
+    { /* ループでコンテナを作成 */
+        /* 月単位のループ */
+        gallery2013Config.map( month => { return (
+            // メインコンテナ
             <div
-                className={ Styles.imageContents }
+                className={ Styles.meinContainer }
             >
-                <img src="" alt="" />
-            </div>
-            {/* テキストコンテンツ */}
-            <div
-                className={ Styles.textContents }
-            >
-                <p>
 
-                </p>
+                {/* 月別タイトル */}
+                <h2
+                    className={ Styles.monthTitle }
+                >
+                    { month[0].month }
+                </h2>
+
+                {/* フレックスコンテナ */}
+                <div
+                    className={ Styles.monthContainer + ' ' + Styles.flexBox }
+                >
+                    { /* コンテンツ単位のループ */
+                        month.map( contents => { return (
+                            <div
+                                className={ Styles.contentsContainer }
+                            >
+                                {/* 画像コンテンツ */}
+                                <div
+                                    className={ Styles.imageContents }
+                                >
+                                    <Img fileName={ contents.fileName } />
+                                </div>
+                                {/*  テキストコンテンツ */}
+                                <div
+                                    className={ Styles.textContents }
+                                >
+                                    <p>
+                                        { contents.comment }
+                                    </p>
+                                </div>
+                            </div>
+                        )})
+                    }
+                </div>
             </div>
-        </div>
-    </div>
+        )})
+    }
   </Layout>
 )
 
