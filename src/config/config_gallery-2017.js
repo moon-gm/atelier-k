83af67b5ee15ac9e3f7d7bbcf134b2de2c0d63d6
akinaga <akinaga@freedomland.jp>
2020-05-24 03:54:09 +0900

    全年代にページ展開

---
+++ src/config/config_gallery-2017.js
@@ -0,0 +1,112 @@
+// 2017年のギャラリーの設定
+export default [
+    [
+        // 1月
+        {
+            'month': 'January',
+            'fileName': 'stamp-1.png',
+            'altName': 'くうちゃんスタンプ画像１',
+            'comment': 'part.1 うさぎかぶり',
+        },
+        {
+            'month': 'January',
+            'fileName': 'stamp-2.png',
+            'altName': 'くうちゃんスタンプ画像１',
+            'comment': 'part.1 うさぎかぶり',
+        },
+        {
+            'month': 'January',
+            'fileName': 'stamp-3.png',
+            'altName': 'くうちゃんスタンプ画像１',
+            'comment': 'part.1 うさぎかぶり',
+        },
+        {
+            'month': 'January',
+            'fileName': 'stamp-4.png',
+            'altName': 'くうちゃんスタンプ画像１',
+            'comment': 'part.1 うさぎかぶり',
+        },
+
+
+    ],
+    [
+        // 2月
+        {
+            'month': 'February',
+            'fileName': 'stamp-1.png',
+            'altName': 'くうちゃんスタンプ画像１',
+            'comment': 'part.1 うさぎかぶり',
+        },
+        {
+            'month': 'February',
+            'fileName': 'stamp-1.png',
+            'altName': 'くうちゃんスタンプ画像１',
+            'comment': 'part.1 うさぎかぶり',
+        },
+        {
+            'month': 'February',
+            'fileName': 'stamp-1.png',
+            'altName': 'くうちゃんスタンプ画像１',
+            'comment': 'part.1 うさぎかぶり',
+        },
+        {
+            'month': 'February',
+            'fileName': 'stamp-1.png',
+            'altName': 'くうちゃんスタンプ画像１',
+            'comment': 'part.1 うさぎかぶり',
+        },
+        {
+            'month': 'February',
+            'fileName': 'stamp-1.png',
+            'altName': 'くうちゃんスタンプ画像１',
+            'comment': 'part.1 うさぎかぶり',
+        },
+    ],
+    [
+        // 3月
+        {
+            'month': 'March',
+            'fileName': 'stamp-1.png',
+            'altName': 'くうちゃんスタンプ画像１',
+            'comment': 'part.1 うさぎかぶり',
+        },
+        {
+            'month': 'March',
+            'fileName': 'stamp-1.png',
+            'altName': 'くうちゃんスタンプ画像１',
+            'comment': 'part.1 うさぎかぶり',
+        },
+        {
+            'month': 'March',
+            'fileName': 'stamp-1.png',
+            'altName': 'くうちゃんスタンプ画像１',
+            'comment': 'part.1 うさぎかぶり',
+        },
+        {
+            'month': 'March',
+            'fileName': 'stamp-1.png',
+            'altName': 'くうちゃんスタンプ画像１',
+            'comment': 'part.1 うさぎかぶり',
+        },
+        {
+            'month': 'March',
+            'fileName': 'stamp-1.png',
+            'altName': 'くうちゃんスタンプ画像１',
+            'comment': 'part.1 うさぎかぶり',
+        },
+        {
+            'month': 'March',
+            'fileName': 'stamp-1.png',
+            'altName': 'くうちゃんスタンプ画像１',
+            'comment': 'part.1 うさぎかぶり',
+        },
+        {
+            'month': 'March',
+            'fileName': 'stamp-1.png',
+            'altName': 'くうちゃんスタンプ画像１',
+            'comment': 'part.1 うさぎかぶり',
+        },
+
+    ],
+];
+
---
+++ src/pages/2014.js
@@ -0,0 +1,118 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import TopBtn from "../components/parts/top-btn"
+
+import { Link } from "gatsby"
+import Img from "../components/parts/image"
+import Config from "../config/config_gallery-2014.js"
+import Styles from "../sass/module/pages/common-gallery.module.scss"
+
+const Gallery2014 = () => (
+  <Layout>
+    <SEO title="Gallery 2014" />
+
+    {/* ページタイトル */}
+    <h1 className="h1">
+        Gallery 2014
+    </h1>
+
+    {/* 月別フォルダタイトル */}
+    <h2 className="h2">
+        Jump to
+    </h2>
+
+    {/* 月別フォルダ */}
+    <div className="flex-all flex-wrap">
+        {/* ループでジャンプする月別フォルダを作成 */
+            Config.map ( month => { return (
+                <div
+                    className={ Styles.monthFolder }
+                    key={ month[0].month }
+                >
+                    <Link
+                        to={ `/2014#${ month[0].month }` }
+                    >
+                        <h3 className={ Styles.monthTitle }>
+                            { month[0].month }
+                        </h3>
+                        <Img fileName="palette-icon.png" />
+                    </Link>
+                </div>
+            )})
+        }
+    </div>
+
+    { /* ループでコンテナを作成 */
+        // 月単位のループ
+        Config.map( month => { return (
+
+            /*--- メインコンテナ ---*/
+            <div
+                className={ Styles.mainContainer }
+                key={ month[0].month }
+            >
+                {/* ヘッダーの固定によるid遷移のズレ対策スペーサー */}
+                <div
+                    className={ Styles.spacerForId }
+                    id={ month[0].month }
+                >
+
+                </div>
+                {/* 月別タイトル */}
+                <h2 className="h2">
+                    { month[0].month }
+                </h2>
+
+                {/*--- 月単位のコンテナ ---*/}
+                <div
+                    className={ `${ Styles.monthContainer } flex-pc flex-wrap` }
+                    key={ month[0].month }
+                >
+                    {// コンテンツ単位のループ
+                        month.map( contents => { return (
+
+                            /*--- コンテンツ単位のコンテナ ---*/
+                            <div
+                                className={ Styles.contentsContainer }
+                                key={ contents.fileName }
+                            >
+                                <Link
+                                    to={ `${ process.env.APP_NAME }` !== "" ? `${ process.env.APP_NAME }/images/${ contents.fileName }` : `/images/${ contents.fileName }` }
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+
+                                    {/* 画像コンテンツ */}
+                                    <div className={ Styles.imageContents }>
+                                        <Img fileName={ contents.fileName } />
+                                    </div>
+
+                                    {/*  テキストコンテンツ */}
+                                    <div className={ Styles.textContents }>
+                                        <p>
+                                            { contents.comment }
+                                        </p>
+                                    </div>
+
+                                </Link>
+                            </div>
+
+                        )})
+                    }
+                </div>
+
+            </div>
+
+        )})
+    }
+
+    <TopBtn
+        classTopBtn="top-btn"
+        getPathName ="/2014"
+    />
+
+  </Layout>
+)
+
+export default Gallery2014
