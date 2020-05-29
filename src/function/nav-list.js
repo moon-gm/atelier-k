c247851d3cab65075f8924696f9cf8487e889849
akinaga <akinaga@freedomland.jp>
2020-05-29 17:13:08 +0900

    documentがエラーとなるためnav-list.jsの処理をコメントアウト

--- src/function/nav-list.js
+++ src/function/nav-list.js
@@ -5,20 +5,21 @@ import Styles from "../sass/module/area/navigation.module.scss"
 // Galleryをタップした時の年代リスト表示処理
 function clickBtnAction(){
     // 年代リスト取得
-    const navList = document.getElementsByClassName("js-nav-btn");
+    // const navList = document.getElementsByClassName("js-nav-btn");
     // 年代リスト表示・非表示処理
     function clickBtn(){
-        Config.map( item => {
-            if (navList[item.no].textContent === item.to){
-                navList[item.no].textContent = "";
-                navList[item.no].style.borderTop = "none";
-                navList[item.no].style.padding = "0";
-            } else {
-                navList[item.no].textContent = item.to;
-                navList[item.no].style.padding = "10px";
-            }
-            return true;
-         })
+        // Config.map( item => {
+        //     if (navList[item.no].textContent === item.to){
+        //         navList[item.no].textContent = "";
+        //         navList[item.no].style.borderTop = "none";
+        //         navList[item.no].style.padding = "0";
+        //     } else {
+        //         navList[item.no].textContent = item.to;
+        //         navList[item.no].style.padding = "10px";
+        //     }
+        //     return true;
+        //  })
+        return true;
     }
 
     // 上記functionを引き継いだonclick属性を付与したGalleryボタンを返す
